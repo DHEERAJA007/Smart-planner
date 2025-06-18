45ca98069db999f5f8d060f7279a0814b5b7b010
DHEERAJA007 <126708107+DHEERAJA007@users.noreply.github.com>
2025-06-18 21:05:13 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Subject(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(100), nullable=False)
-    hours = db.Column(db.Integer, nullable=False)
